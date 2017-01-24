6314c1c699ff5a10c73d255415e20f45352bff0d
Okan Soyturk <okasoyturk@emi.u-bordeaux1.fr>
2017-01-24 15:00:35 +0100

    Page d'accueil + navbar + bootstrap + fichiers log et signup a faire

---
+++ navbar.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">WebSiteName</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Page d'accueil</a></li>
+      <li><a href="#">Mes demandes(0)</a></li>
+      <li><a href="#">Mes offres(0)</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="signup.php"><span class="glyphicon glyphicon-user"></span> S'inscire</a></li>
+      <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Se connecter</a></li>
+    </ul>
+  </div>
+</nav>
