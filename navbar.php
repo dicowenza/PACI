6d16fed27bc5742cddcc9033f060399264c5fe0b
Okan Soyturk <okasoyturk@emi.u-bordeaux1.fr>
2017-01-27 14:41:32 +0100

    navbar redef

--- navbar.php
+++ navbar.php
@@ -1,16 +1,16 @@
-<nav class="navbar navbar-inverse">
+<nav class="navbar navbar-inverse" style="padding : 10px">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="index.php">Pacidnah</a>
+      <a style="font-size: 20pt ! important;" class="navbar-brand" href="index.php">Pacidnah</a>
     </div>
     <ul class="nav navbar-nav">
-      <li class="active"><a href="index.php">Page d'accueil</a></li>
-      <li><a href="#">Mes demandes(0)</a></li>
-      <li><a href="#">Mes offres(0)</a></li>
+      <li class="active" style="font-size: 20pt ! important;"><a href="index.php">Page d'accueil</a></li>
+      <li><a style="font-size: 20pt ! important;" href="#">Mes demandes(0)</a></li>
+      <li><a style="font-size: 20pt ! important;" href="#">Mes offres(0)</a></li>
     </ul>
     <ul class="nav navbar-nav navbar-right">
-      <li><a href="signup.php"><span class="glyphicon glyphicon-user"></span> S'inscire</a></li>
-      <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Se connecter</a></li>
+      <li><a style="font-size: 20pt ! important;" href="signup.php"><span class="glyphicon glyphicon-user"></span> S'inscire</a></li>
+      <li><a style="font-size: 20pt ! important;" href="login.php"><span class="glyphicon glyphicon-log-in"></span> Se connecter</a></li>
     </ul>
   </div>
 </nav>
