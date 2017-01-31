541f3b7d78852c72b6cdef3538dfea529fd16e07
Okan Soyturk <okasoyturk@emi.u-bordeaux1.fr>
2017-01-31 14:52:35 +0100

    formulaire ajouter service

--- servicesView.php
+++ servicesView.php
@@ -12,6 +12,7 @@
 <script src="bootstrap/js/bootstrap.min.js"></script>
 
 <script type="text/javascript">
+  
 </script>
 
 <body text-align="center">
@@ -54,13 +55,46 @@
 
           </div>
         </div>';
-
       }
 
     ?>
 
     <br>
-    <button style="font-size: 35px ! important; width: 500px; height: 100px;" class="btn btn-success" type="button">PROPOSER MES SERVIVES</button>
+    <button style="font-size: 35px ! important; width: 500px; height: 100px;" class="btn btn-success" type="button" data-toggle="modal" data-target="#addServiceForm" >PROPOSER MES SERVICES</button>
+    <div id="addServiceForm" class="modal fade" role="dialog">
+      <div class="modal-dialog">
+        <!-- Modal content-->
+        <div class="modal-content">
+          <div class="modal-header">
+            <h4 class="modal-title" style="font-size: 23pt ! important;">JE ME PROPOSE</h4>
+          </div>
+          <div class="modal-body">
+             <div>
+               <form class="addService">
+               <fieldset>
+                <br><ul class="nav nav-list">
+                 <li style="font-size: 18pt ! important;" class="nav-header"><u><b>Catégorie</b></u></li><br>
+                 <li>
+                    <label style="font-size: 15pt ! important;" class="radio-inline"><input type="radio" name="optradio">Aide à domicile</label>
+                    <label style="font-size: 15pt ! important;" class="radio-inline"><input type="radio" name="optradio">Soins infirmiers</label>
+                    <label style="font-size: 15pt ! important;" class="radio-inline"><input type="radio" name="optradio">Loisirs</label> 
+                 </li><br><br>
+                 <li style="font-size: 18pt ! important;" class="nav-header"><u><b>Titre</b></u></li><br>
+                 <li><input style="font-size: 18pt ! important;" class="input-xlarge" type="text" name="title"></li><br><br>
+                 <li style="font-size: 18pt ! important;" class="nav-header"><u><b>Description</b></u></li><br>
+                 <li><textarea style="font-size: 18pt ! important;" class="input-xlarge" name="sug" rows="5"></textarea></li>
+                </ul><br><br>
+               </fieldset>
+               </form>
+          </div>
+          <div class="modal-footer">
+            <button class="btn btn-success" id="submit">Envoyer</button>
+            <button href="#" class="btn btn-danger" data-dismiss="modal">Fermer</button>
+          </div>
+        </div>
+
+      </div>
+    </div>
     <br><br>
 
   </div>
