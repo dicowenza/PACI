e9db6a9155e0853f61fd228443d9dc67e4a28089
Okan Soyturk <okasoyturk@emi.u-bordeaux1.fr>
2017-02-10 15:06:59 +0100

    reponse service avec mail OK

--- sendMail.php
+++ sendMail.php
@@ -1,21 +1,11 @@
 <?php
 
-/*$to      = 'okan.soyturk@etu.u-bordeaux.fr';
-$subject = 'Ici le sujet';
-$message = 'le ptit message ici';
-$headers = 'From: pacidnah@salut.com' . "\r\n" .
-    'Reply-To: pacidnah@salut.com' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-
-mail($to, $subject, $message, $headers);*/
-
 $to      = $_POST['destination'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
-$headers = $_POST['sender'] . "\r\n" .
-    'Reply-To:'.$_POST['sender'] . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
+$headers = 'From:'.$_POST['sender']."\r\n";
+$headers .='Reply-To:'.$_POST['sender']."\r\n";
+$headers .='X-Mailer: PHP/'.phpversion();
 
 mail($to, $subject, $message, $headers);
-
 ?> 
