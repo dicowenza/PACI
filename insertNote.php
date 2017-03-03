89d789d45baa8ccabd57982e0c3e8cc308e4752e
Okan Soyturk <okasoyturk@emi.u-bordeaux1.fr>
2017-03-03 16:13:44 +0100

    vote unique par user et reponse

--- insertNote.php
+++ insertNote.php
@@ -1,26 +1,25 @@
 <?php
 session_start();
-/*if (empty($_POST['nom']) || empty($_POST['prenom']) || empty($_POST['password']) || empty($_POST['pseudo']) || empty($_POST['email'])){
-    header('Location: signup.php?empty_fields=true');
-    break;
-}*/
 
-try
-{
+try{
 	$bdd = new PDO('mysql:host=dbserver;dbname=test;charset=utf8', 'jefseutin', 'jefco');
 }
-catch (Exception $e)
-{
+catch (Exception $e){
         die('Erreur : ' . $e->getMessage());
 }
-	$req = $bdd->prepare("INSERT INTO note VALUES (0, :answerID, :userID, :status)");
-    $req->execute(array(
-            'answerID' => $_GET['answerID'],
-            'userID' => $_GET['userID'],
-            'status' => $_GET['noteStatus']
-            ));
 
-    $data = $req->fetchAll();
+$req = $bdd->prepare("SELECT * FROM note WHERE note_user_ID = ".$_GET['userID']." AND note_answer_ID = ".$_GET['answerID']); $req->execute();
 
-   header('Location: questionsView.php');
+if($req->rowCount() == 0)
+    $req = $bdd->prepare("INSERT INTO note VALUES (0, :answerID, :userID, :status)");
+else
+    $req = $bdd->prepare("UPDATE note SET note_status = :status WHERE note_answer_ID = :answerID AND note_user_ID = :userID");
+
+$req->execute(array(
+        'answerID' => $_GET['answerID'],
+        'userID' => $_GET['userID'],
+        'status' => $_GET['noteStatus']
+        ));
+
+header('Location: questionsView.php');
 ?>
